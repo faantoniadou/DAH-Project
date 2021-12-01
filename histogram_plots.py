abe49af164f736cb56f544e6dbe6da80f488f459
Faidra Antoniadou <63123583+faantoniadou@users.noreply.github.com>
2021-12-01 10:57:10 +0000

    Update and rename codef1_fedra.py to histogram_plots.py

--- histogram_plots.py
+++ histogram_plots.py
@@ -10,6 +10,7 @@ This code plots the mass, transverse momentum of muon pair, rapidity,
 momentum of the muon pair, then the transverse momenta of each of the particles
 No of bins is calculated using the Freeman-Diaconis rule
 """
+
 import  numpy  as  np
 import pylab
 from scipy import stats
@@ -24,7 +25,7 @@ nevent  =  int(len(datalist)/6)
 xdata  =  np.array(np.split(datalist,nevent))
 
 
-#%%
+
 def plot_histogram(name, values, units, min, max)  :     
     #find binwidth, use Freeman-Diaconis rule
     mass_iqr = stats.iqr(xmass)
@@ -37,7 +38,7 @@ def plot_histogram(name, values, units, min, max)  :
     pylab.xlim(np.min(values))
     pylab.show()
 
-#%%
+
 #  make  list  of  invariant  mass  of  events
 xmass_name = r'$Mass ( \mu ^-\mu ^+)$ $(GeV/c^2)$'
 xmass_units = r'Candidates/ (25 Mev$/c^2$)'
@@ -47,7 +48,7 @@ xmass = xdata[:,0]
 plot_histogram(xmass_name, xmass, xmass_units, np.min(xmass), np.max(xmass))
 
 
-#%%
+
 #make list of transverse momenta of muon pair
 trans_name = r'Transverse momentum of the muon pair'
 mom_units = r'Candidates/ (25 Mev$/c^2$)'
@@ -56,7 +57,7 @@ trans_momentum_pair = xdata[:,1]
 plot_histogram(trans_name, trans_momentum_pair, mom_units, 0, 30)
 
 
-#%%          
+          
 #make list of rapidities of muon pair
 
 rapidity_name = "Rapidity"
@@ -67,21 +68,21 @@ rapidity = xdata[:,2]
 plot_histogram(rapidity_name, rapidity, rapidity_units, np.min(rapidity), np.max(rapidity))
 
 
-#%%
+
 
 mom_pair_name = "Momentum of muon pair"
 momentum_pair = xdata[:,3]
 
 plot_histogram(mom_pair_name, momentum_pair, mom_units, np.min(momentum_pair), 700)
 
-#%%
+
 trans_first_name = "Transverse momentum of first muon"
 trans_momentum_first = xdata[:,4])
 
 plot_histogram(trans_first_name, trans_momentum_first, mom_units, np.min(trans_momentum_first), 25)
 
 
-#%%
+
 trans_second_name = "Transverse momentum of second muon"
 trans_momentum_second = xdata[:, 5]
 
